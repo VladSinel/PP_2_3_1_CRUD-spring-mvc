3c76799f0d0a2f8102f31d21f01c361cd287a3c4
Vladislav Sinelnikov <jobnv86@gmail.com>
2022-06-17 18:14:50 +0500

    Исправление: удалено @Transactional из DAO

--- src/main/java/web/dao/UserDAOImp.java
+++ src/main/java/web/dao/UserDAOImp.java
@@ -2,9 +2,7 @@ package web.dao;
 
 
 import org.springframework.stereotype.Repository;
-import javax.transaction.Transactional;
 import web.model.User;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
@@ -15,31 +13,26 @@ public class UserDAOImp implements UserDAO {
     @PersistenceContext
     private EntityManager entityManager;
 
-    @Transactional
     @Override
     public List<User> allUsers() {
         return entityManager.createQuery("from User", User.class).getResultList();
     }
 
-    @Transactional
     @Override
     public void addUser(User user) {
         entityManager.persist(user);
     }
 
-    @Transactional
     @Override
     public void editUser(int id, User user) {
         entityManager.merge(user);
     }
 
-    @Transactional
     @Override
     public void deleteUser(int id) {
         entityManager.remove(getUser(id));
     }
 
-    @Transactional
     @Override
     public User getUser(int id) {
         return entityManager.find(User.class, id);
